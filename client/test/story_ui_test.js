86c34dc44e1ded25587f24f4e341ab9f9f817d66
Tim Ruffles <oi@truffles.me.uk>
2014-03-22 13:23:48 +0000

    defined first set of story UI tests

--- client/test/story_ui_test.js
+++ client/test/story_ui_test.js
@@ -2,9 +2,64 @@ describe("Story UI",function() {
 
   describe("viewing stories",function() {
 
-    it("stories are visible")
+    var storyView;
 
-    it("stories are sorted by score")
+    before(function() {
+      storyView = new StoryView({
+        collection: [
+          {
+            id: 123,
+            title: "Why we rewrote our startup in Lisp",
+            score: 10,
+            url: "http://pg-fanppl.com/lisp"
+          },
+          {
+            id: 600,
+            title: "Learn to code in your tea break, by someone who's never coded",
+            score: 20,
+            url: "http://www2.tories.org.uk"
+          }
+        ]
+      })
+    })
+
+    it("stories are visible",function() {
+      assert.equal($(".story",storyView.el).length,2)
+    })
+
+    it("displays titles",function() {
+      // using .text() to ignore the HTML structure
+      assert.match(storyEl(123).text(),getStory(123).title);
+    })
+
+    it("links stories to URLs",function() {
+      var withUrl = storyEl(123).find("[href=" + getStory(123).url + "]");
+      assert.equal(withUrl.length,1);
+    })
+
+    it("displays scores",function() {
+      var number = parseInt(storyEl(123).find(".score").val());
+      assert.equal(number,getStory(123).score);
+    })
+
+    it("sorts stories by score, highest to lowest",function() {
+      var ids = $("[data-id]",storyView.el).map(function(i,el) {
+        return el.attr("data-id")
+      });
+      assert.equal([600,123],ids);
+    })
+
+    // test helper functions, so we can change the implementation
+    // of the view without having to change all of our tests
+    // (e.g how do we get an el for a story, this might
+    // change if we move to a collection view/item view collab, or 
+    // we changed the collection data-structure).
+    function storyEl(id) {
+      return $("[data-id=" + id + "]",storyView.el);
+    }
+    function getStory(id) {
+      return _.findWhere(storyEl.collection,{id: id});
+    }
 
   })
   
