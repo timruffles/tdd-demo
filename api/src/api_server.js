4d40b7a02901db90d21b0a798e3f6863bc6416c4
Tim Ruffles <oi@truffles.me.uk>
2014-03-22 17:01:20 +0000

    submission tests in place

---
+++ api/src/api_server.js
@@ -0,0 +1,6 @@
+var express = require("express");
+
+module.exports = function apiServer(dependencies) {
+  var app = express();
+  return app;
+}
--- api/test/stories_api_test.js
+++ api/test/stories_api_test.js
@@ -1,18 +1,53 @@
+var server = require("../src/api_server.js");
+var supertest = require("supertest");
+
 describe("Story API",function() {
+
+  var app;
+  var request;
+  beforeEach(function() {
+    app = server({});
+    request = supertest(app);
+  })
 
   describe("creating stories",function() {
 
-    it("creates a story on valid submission")
+    it("creates a story on valid submission",function(done) {
+      submit({
+          title: "Why Javascript is like a burrito",
+          url: "http://pseudtasia.cc"
+        })
+        .expect(200)
+        .expect(function(res) {
+          assert.isNumber(res.body.id)
+        })
+        .end(done);
+    })
 
-    it("rejects stories with invalid URLS")
+    it("rejects stories with invalid URLS",function(done) {
+      submit({
+          title: "Why Javascript is like a burrito",
+          url: "this isn't a URL"
+        })
+        .expect(400)
+        .end(done);
+    })
+
+    function submit(story) {
+      return request.post("/stories")
+        .set('Content-Type', 'application/json')
+        .send(story);
+    }
 
   })
   
   describe("voting on stories",function() {
 
-    it("upvoting a story increases score")
+    it("upvoting a story increases score",function() {
+    })
 
-    it("downvoting a story decreases score")
+    it("downvoting a story decreases score",function() {
+    })
 
   })
 
