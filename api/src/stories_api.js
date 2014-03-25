d5c90213f6f90daad0b626ab93b0e33e64287061
Tim Ruffles <oi@truffles.me.uk>
2014-03-25 09:59:24 +0000

    empty out implementation

--- api/src/stories_api.js
+++ api/src/stories_api.js
@@ -1,30 +1,5 @@
 var _ = require("underscore");
 
 module.exports = function(app) {
-
-  var storyId = 1;
-  var stories = {
-  };
-
-  app.post("/stories",function(req,res) {
-
-    var story = _.pick(req.body,"title","url");
-    if(!validUrl(story.url)) {
-      return res.send(400,"Invalid URL");
-    }
-
-    story.id = storyId;
-
-    storyId += 1; 
-
-    res.send(story);
-
-
-  });
-
-  function validUrl(url) {
-    // obviously not very accurate :)
-    return /^http/.test(url);
-  }
-
+  // add express actions to `app` here
 }
